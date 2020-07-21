daeb2b3404e34cedf33636ca93f633d5493f0d7e
muhammad awais <awaisshaikh94@gmail.com>
2020-07-22 02:29:10 +0500

    basic form populated with data some fixes remaining

--- src/app/pages/form-direct-deposit-pdf-making/form-direct-deposit-pdf-making.component.ts
+++ src/app/pages/form-direct-deposit-pdf-making/form-direct-deposit-pdf-making.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-direct-deposit-pdf-making',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormDirectDepositPdfMakingComponent implements OnInit {
 
-  constructor() { }
+  routerData: any = history.state;
+  data: any;
+  dateToday: any = new Date();
+
+  page1: any = 'https://i.ibb.co/DMxm45T/0001.jpg';
+
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.getFormData();
+  }
+
+  print() {
+    window.print();
+  }
+
+  getFormData() {
+    if (this.routerData.data) {
+      this.data = this.routerData.data[0];
+      console.log("data", this.data);
+    } 
+    else {
+      this.router.navigateByUrl('/employees');
+    }
   }
 
 }
--- src/app/pages/form-i9-pdf-making/form-i9-pdf-making.component.ts
+++ src/app/pages/form-i9-pdf-making/form-i9-pdf-making.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-i9-pdf-making',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormI9PdfMakingComponent implements OnInit {
 
-  constructor() { }
+  routerData: any = history.state;
+  data: any;
+  dateToday: any = new Date();
+
+  page1: any = 'https://i.ibb.co/2gJJfZx/0001.jpg';
+  page2: any = 'https://i.ibb.co/y4bZN2s/0002.jpg';
+  page3: any = 'https://i.ibb.co/bzzRBGb/0003.jpg';
+
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.getFormData();
+  }
+
+  print() {
+    window.print();
+  }
+
+  getFormData() {
+    if (this.routerData.data) {
+      this.data = this.routerData.data[0];
+      console.log("data", this.data);
+    } 
+    else {
+      this.router.navigateByUrl('/employees');
+    }
   }
 
 }
