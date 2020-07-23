c182cda4c5a2ffe60f8d46d560150ddd1b798a79
muhammad awais <awaisshaikh94@gmail.com>
2020-07-23 20:43:41 +0500

    environment added prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_BASE_URL: 'http://speedx.citrixco.co.uk/employee-system-php-core-api',
-  FRONTEND_BASE_URL: 'http://speedx.citrixco.co.uk/',
+  FRONTEND_BASE_URL: 'http://speedx.citrixco.co.uk',
 };
